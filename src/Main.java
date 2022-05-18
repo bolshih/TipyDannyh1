e91e1098f598443960311acd2cd3b7cd5dcf99c4
boshih <artem.bolshih@mail.ru>
2022-05-18 18:55:22 +0300

    start

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int tPrice = 2500, mPrice = 20;
+
+        int miles = tPrice / mPrice;
+
+        System.out.println("Количество миль: " + miles);
+    }
+}
